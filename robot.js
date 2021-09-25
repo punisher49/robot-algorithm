777f6e04b08b83b2eb28670dba6542f1ebfc4847
Hasan Aliyev <al>
2021-09-25 16:17:36 +1000

    adding test cases, fixing bug and refactor

--- robot.js
+++ robot.js
@@ -34,43 +34,29 @@ class Robot {
     this.facing = direction;
     this.robotsNum += 1;
     this.robotsList.push(this.robotsNum);
-    this.robotData.set(`Robot${this.robotsNum}`, {
-      coordinates: this.coordinates,
-      facing: this.facing,
-      robotId: this.robotsNum,
-    });
+    this.#SET_ROBOTS_LOCATION();
   }
 
   MOVE_FORWARD() {
     if (!this.coordinates || !this.facing)
       throw new WrongInput(`Please create Robot First`);
-
     if (this.facing === "NORTH") {
       if (this.coordinates[1] + 1 > UNIT - 1) return;
-      return (this.coordinates = [
-        this.coordinates[0],
-        this.coordinates[1] + 1,
-      ]);
+      this.coordinates = [this.coordinates[0], this.coordinates[1] + 1];
+      this.#SET_ROBOTS_LOCATION();
     } else if (this.facing === "EAST") {
       if (this.coordinates[0] + 1 > UNIT - 1) return;
-      return (this.coordinates = [
-        this.coordinates[0] + 1,
-        this.coordinates[1],
-      ]);
+      this.coordinates = [this.coordinates[0] + 1, this.coordinates[1]];
+      this.#SET_ROBOTS_LOCATION();
     } else if (this.facing === "SOUTH") {
       if (this.coordinates[1] - 1 < 0) return;
-      return (this.coordinates = [
-        this.coordinates[0],
-        this.coordinates[1] - 1,
-      ]);
+      this.coordinates = [this.coordinates[0], this.coordinates[1] - 1];
+      this.#SET_ROBOTS_LOCATION();
     } else if (this.facing === "WEST") {
       if (this.coordinates[0] - 1 < 0) return;
-      return (this.coordinates = [
-        this.coordinates[0] - 1,
-        this.coordinates[1],
-      ]);
+      this.coordinates = [this.coordinates[0] - 1, this.coordinates[1]];
+      this.#SET_ROBOTS_LOCATION();
     }
-    console.log(this.coordinates);
   }
 
   ROTATE_LEFT() {
@@ -78,11 +64,12 @@ class Robot {
       throw new WrongInput(`Please create Robot First`);
     // rotate 90 degrees leave the same position
     // (i.e.) if current position is north, then change it to south
-    return (this.facing =
+    this.facing =
       DIRECTIONS[
         (DIRECTIONS.indexOf(this.facing) - 1 + DIRECTIONS.length) %
           DIRECTIONS.length
-      ]);
+      ];
+    this.#SET_ROBOTS_LOCATION();
   }
 
   ROTATE_RIGHT() {
@@ -90,33 +77,31 @@ class Robot {
       throw new WrongInput(`Please create Robot First`);
     // rotate 90 degrees leave фе the same position
     // i.e. if facing NORTH then change it to EAST
-    return (this.facing =
-      DIRECTIONS[(DIRECTIONS.indexOf(this.facing) + 1) % DIRECTIONS.length]);
+    this.facing =
+      DIRECTIONS[(DIRECTIONS.indexOf(this.facing) + 1) % DIRECTIONS.length];
+    this.#SET_ROBOTS_LOCATION();
   }
 
   REPORT_LOCATION() {
     if (!this.coordinates || !this.facing)
       throw new WrongInput(`Please create Robot First`);
     // report coordinates and where robot is facing at the moment
-    console.log(
-      `Robot${this.robotsNum}'s coordinates are [${this.coordinates}] and facing ${this.facing}`
-    );
-    console.log(`There are ${this.robotsList.length} robot/s in the system`);
+    const report = `Robot${this.robotsNum}'s coordinates are [${this.coordinates}], facing ${this.facing} and There are ${this.robotsList.length} robot/s in the system`;
+    return report;
   }
 
   CHANGE_ROBOT(id) {
-    console.log("----------------CHANGING ROBOT ON TABLE----------------");
-    if (id > this.robotsList.length) {
+    if (!id || id > this.robotsList.length) {
       throw new WrongInput(
         `There are only ${this.robotsList.length} robot/s in database`
       );
     }
     this.#CLEAR_ALL();
+    console.log("----------------CHANGING ROBOT ON TABLE----------------");
     const { coordinates, facing, robotId } = this.robotData.get(`Robot${id}`);
     this.coordinates = coordinates;
     this.facing = facing;
     this.robotsNum = robotId;
-    console.log(this.coordinates, this.facing, this.robotsNum);
   }
 
   #CLEAR_ALL() {
@@ -125,68 +110,14 @@ class Robot {
     this.facing = null;
     this.robotsNum = null;
   }
+
+  #SET_ROBOTS_LOCATION() {
+    this.robotData.set(`Robot${this.robotsNum}`, {
+      coordinates: this.coordinates,
+      facing: this.facing,
+      robotId: this.robotsNum,
+    });
+  }
 }
 
 module.exports = Robot;
-
-const robot = new Robot();
-
-robot.PLACE_ROBOT(0, 0, "WEST");
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-robot.REPORT_LOCATION();
-
-robot.ROTATE_LEFT();
-robot.MOVE_FORWARD();
-robot.REPORT_LOCATION();
-
-robot.ROTATE_LEFT();
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-robot.MOVE_FORWARD();
-
-robot.REPORT_LOCATION();
-
-robot.PLACE_ROBOT(3, 3, "WEST");
-robot.REPORT_LOCATION();
-
-robot.PLACE_ROBOT(2, 2, "WEST");
-robot.REPORT_LOCATION();
-
-robot.PLACE_ROBOT(1, 1, "WEST");
-robot.REPORT_LOCATION();
-
-// robot.PLACE_ROBOT(3, 3,'NORTH');
-// robot.REPORT_LOCATION()
-
-// robot.PLACE_ROBOT(1, 2, 'EAST');
-// robot.REPORT_LOCATION()
-
-// robot.PLACE_ROBOT(3, 4, 'WEST');
-// robot.REPORT_LOCATION()
-
-// robot.PLACE_ROBOT(2, 1, 'NORTH');
-// robot.REPORT_LOCATION()
-
-// robot.PLACE_ROBOT(3, 0, 'SOUTH');
-// robot.REPORT_LOCATION()
-
-// robot.ROBOT(2)
-// robot.REPORT_LOCATION()
-
-// robot.ROBOT(3)
-// robot.REPORT_LOCATION()
-
-// robot.ROBOT(2)
-// robot.REPORT_LOCATION()
-
-// robot.ROBOT(4)
-// robot.REPORT_LOCATION()
-
-// robot.ROBOT(5)
-// robot.REPORT_LOCATION()
--- test/robot.test.js
+++ test/robot.test.js
@@ -1,13 +1,127 @@
 const expect = require("chai").expect;
 const Robot = require("../robot");
+const robot = new Robot();
 
-describe("Testing all Robot class methods", () => {
-  it("creates a robot on the table facing NORTH and moving forward", () => {
-    const robot = new Robot();
-    // robot.PLACE_ROBOT(0, 0, "NORTH");
-    // robot.MOVE_FORWARD();
-    // expect(robot.REPORT_LOCATION()).to.equal(
-    //   `Robot1's coordinates are [0,1] and facing NORTH`
-    // );
+describe("Test all error cases", () => {
+  it.only("Expect PLACE_ROBOT to throw an error if no arguments have been passed", () => {
+    expect(() => {
+      robot.PLACE_ROBOT();
+    }).to.throw();
+  });
+
+  it.only("Expect MOVE_FORWARD function to throw an error if it has sbeen called before PLACE_ROBOT", () => {
+    expect(() => {
+      robot.MOVE_FORWARD();
+    }).to.throw();
+  });
+
+  it.only("Expect ROTATE_LEFT function to throw an error if it has sbeen called before PLACE_ROBOT", () => {
+    expect(() => {
+      robot.ROTATE_LEFT();
+    }).to.throw(Error);
+  });
+
+  it.only("Expect ROTATE_RIGHT function to throw an error if it has sbeen called before PLACE_ROBOT", () => {
+    expect(() => {
+      robot.ROTATE_RIGHT();
+    }).to.throw(Error);
+  });
+
+  it.only("Expect REPORT_LOCATION function to throw an error if it has sbeen called before PLACE_ROBOT", () => {
+    expect(() => {
+      robot.REPORT_LOCATION();
+    }).to.throw(Error);
+  });
+
+  it.only("Expect CHANGE_ROBOT function to throw an error if no valid value has been passed", () => {
+    expect(() => {
+      robot.CHANGE_ROBOT();
+    }).to.throw(Error);
+  });
+});
+
+describe("Testing robot's functions", () => {
+  it.only("creates Robot1 on the table with initial location [0, 0], facing NORTH and moving forward, expecting result: [0,1] NORTH", () => {
+    robot.PLACE_ROBOT(0, 0, "NORTH");
+    robot.MOVE_FORWARD();
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot1's coordinates are [0,1], facing NORTH and There are 1 robot/s in the system`
+    );
+  });
+
+  it.only("creates Robot2 on the table with initial location [0, 0], facing NORTH and rotating LEFT, expecting result: [0,0] WEST", () => {
+    robot.PLACE_ROBOT(0, 0, "NORTH");
+    robot.ROTATE_LEFT();
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot2's coordinates are [0,0], facing WEST and There are 2 robot/s in the system`
+    );
+  });
+
+  it.only(`creates Robot3 on the table with initial location [1, 2] facing EAST, Moving forward twice, moving left once and moving forward once again, expecting result: [0,0] WEST`, () => {
+    robot.PLACE_ROBOT(1, 2, "EAST");
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.ROTATE_LEFT();
+    robot.MOVE_FORWARD();
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot3's coordinates are [3,3], facing NORTH and There are 3 robot/s in the system`
+    );
+  });
+  it.only("creates Robot4 and preventing robot from falling off the table after calling MOVE FORWARD function 5 times", () => {
+    robot.PLACE_ROBOT(0, 0, "NORTH");
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot4's coordinates are [0,4], facing NORTH and There are 4 robot/s in the system`
+    );
+  });
+  it.only("creates Robot5 and preventing robot from falling off the table after calling MOVE FORWARD function 5 times", () => {
+    robot.PLACE_ROBOT(1, 2, "EAST");
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    robot.MOVE_FORWARD();
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot5's coordinates are [4,2], facing EAST and There are 5 robot/s in the system`
+    );
+  });
+
+  it.only("Bringing Robot number 2 to the Table", () => {
+    robot.CHANGE_ROBOT(2);
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot2's coordinates are [0,0], facing WEST and There are 5 robot/s in the system`
+    );
+  });
+
+  it.only("Bringing Robot number 3 to the Table", () => {
+    robot.CHANGE_ROBOT(3);
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot3's coordinates are [3,3], facing NORTH and There are 5 robot/s in the system`
+    );
+  });
+
+  it.only("Bringing Robot number 4 to the Table", () => {
+    robot.CHANGE_ROBOT(4);
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot4's coordinates are [0,4], facing NORTH and There are 5 robot/s in the system`
+    );
+  });
+
+  it.only("Bringing Robot number 5 to the Table", () => {
+    robot.CHANGE_ROBOT(5);
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot5's coordinates are [4,2], facing EAST and There are 5 robot/s in the system`
+    );
+  });
+
+  it.only("Bringing Robot number 1 to the Table", () => {
+    robot.CHANGE_ROBOT(1);
+    expect(robot.REPORT_LOCATION()).to.equal(
+      `Robot1's coordinates are [0,1], facing NORTH and There are 5 robot/s in the system`
+    );
   });
 });
